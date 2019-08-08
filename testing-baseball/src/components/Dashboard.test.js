a8dd7a877ac4bd0e3010877e8943337da3b77c72
Virginia Thayer <virginia@stripe.com>
2019-08-08 13:12:23 -0700

    set up project: created new react app, added Display and Dashboard components and corresponding test files with empty tests in them to start.

---
+++ testing-baseball/src/components/Dashboard.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { render } from '@testing-library/react';
+import "@testing-library/react/cleanup-after-each";
+import Dashboard from './Dashboard';
+
+describe('Dashboard component', () => {
+    it('should do something', () => {
+        // expect
+    })
+})
---
+++ testing-baseball/src/components/Display.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { render } from '@testing-library/react';
+import "@testing-library/react/cleanup-after-each";
+import Display from './Display';
+
+describe('Display component', () => {
+    it('should display things', () ={
+        //expect
+    })
+})
